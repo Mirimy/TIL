60215504f8453710958ff2c80861e82e21fc56e9
Mirimy <6789aaaaa@gmail.com>
2022-09-01 23:25:24 +0900

    0901 TIL

--- 0831/Workshop_CRUD/articles/urls.py
+++ 0831/Workshop_CRUD/articles/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 app_name = 'articles'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('new/', views.new, name='new'),
+    path('index/', views.index, name='index'),
+    # path('new/', views.new, name='new'),
     path('create/', views.create, name='create'),
+    path('detail/<int:article_pk>/', views.detail, name='detail'),
+    # path('edit/<int:article_pk>/', views.edit, name='edit'),
+    path('update/<int:article_pk>/', views.update, name='update'),
+    path('delete/<int:article_pk>/', views.delete, name='delete'),
 ]
--- 0831/Workshop_CRUD/articles/views.py
+++ 0831/Workshop_CRUD/articles/views.py
@@ -1,28 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Article
 
 # Create your views here.
 def index(request) :
-    articles = Article.objects.all()
+    articles = Article.objects.all()[::-1]
     context = {
         'articles' : articles,
     }
 
     return render(request, 'articles/index.html', context)
 
-def new(request) :
-    return render(request, 'articles/new.html')
+# def new(request) :
+#     return render(request, 'articles/new.html')
 
 def create(request) :
-    title = request.GET.get('title')
-    content = request.GET.get('content')
+    if request.method == 'POST' :
+        title = request.POST.get('title')
+        content = request.POST.get('content')
 
-    article = Article(title=title, content=content)
-    article.save()
+        article = Article(title=title, content=content)
+        article.save()
 
-    articles = Article.objects.all()
+        return redirect('articles:detail', article.pk)
+    return render(request, 'articles/new.html')
+
+def detail(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
     context = {
-        'articles' : articles,
+        'article': article,
     }
+    return render(request, 'articles/detail.html', context)
 
-    return render(request, 'articles/index.html', context)
+# def edit(request, article_pk):
+#     article = Article.objects.get(pk=article_pk)
+#     context = {
+#         'article': article
+#     }
+#     return render(request, 'articles/edit.html', context)
+
+def update(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if request.method == 'POST' :
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        article.title = title
+        article.content = content
+        article.save()
+        return redirect('articles:detail', article_pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/edit.html', context)
+
+def delete(request, article_pk):
+    if request.method == 'POST' :
+        article = Article.objects.get(pk=article_pk)
+        article.delete()
+
+    return redirect('articles:index')
